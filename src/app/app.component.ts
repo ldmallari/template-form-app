563b98993cfb871947e5668bd35db12321573796
Yuji <mallarilevin@gmail.com>
2025-02-17 12:00:50 +0800

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'template-form-app';
+  anytext: string = "";
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
 }
